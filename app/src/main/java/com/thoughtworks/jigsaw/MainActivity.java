aaa11555dbe974edbb3df68b2bdb1795158f249a
hemantksingh <hemantksingh.hk@gmail.com>
2015-02-10 12:36:39 +0000

    Display Dashboard view on login.

--- app/src/main/java/com/thoughtworks/jigsaw/MainActivity.java
+++ app/src/main/java/com/thoughtworks/jigsaw/MainActivity.java
@@ -1,17 +1,43 @@
 package com.thoughtworks.jigsaw;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private MainController controller;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.controller = new MainController();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                handleLogin();
+            }
+        });
+    }
+
+    private void handleLogin() {
+        if(this.controller.isAuthenticated()) {
+            Intent intent = new Intent(this, DashboardActivity.class);
+            startActivity(intent);
+
+            Log.wtf("Main activity", "Logged in..." );
+        }
     }
 
 
---
+++ app/src/main/java/com/thoughtworks/jigsaw/MainController.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.jigsaw;
+
+/**
+ * Created by hkumar on 09/02/2015.
+ */
+public class MainController {
+    public boolean isAuthenticated() {
+        return true;
+    }
+}
